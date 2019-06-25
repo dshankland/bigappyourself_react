60ae7cee30b6a2a4eda3f805434bf687950f14ea
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-06-25 23:16:52 +0100

    fixes tweet button and input in compliments container, tweaks text on Home.js and LoginFormCOntainer

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = (props) => {
       <h1 className="login"><img src="../../favicon.ico" alt="Big App Yourself" width="200" height="200" align="center"/>
       Big App Yourself!</h1>
     </div>
-      <h2 className="login">Randomly generated compliments, but 100% sincere!</h2>
+      <h2 className="login">Compliment engine, guaranteed 100% sincere</h2>
 
       <LoginFormContainer/>
     </div>
