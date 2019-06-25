737a54ed763ca637d9c3552558d2a6d866bd408b
JMMoir <jmmoir@outlook.com>
2019-06-25 16:18:50 +0100

    css and twitter feed added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import LoginFormContainer from '../containers/LoginFormContainer';
+import '../App.css'
 
 const Home = (props) => {
 
   return (
     <div>
-      <h1>You made a great choice coming here!</h1>
+    <div id="header">
+      <h1><img src="../../favicon.ico" width="200" height="200" align="center"/>
+      Big App Yourself!</h1>
+    </div>
+      <h2>Randomly generated compliments, but 100% sincere!</h2>
+
       <LoginFormContainer/>
     </div>
   )
