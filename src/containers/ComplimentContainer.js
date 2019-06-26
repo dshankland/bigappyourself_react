a8a5c387ba109481d927dedaf910a1e121861a1f
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-06-26 10:55:02 +0100

    final styling tweaks

--- src/containers/ComplimentContainer.js
+++ src/containers/ComplimentContainer.js
@@ -53,22 +53,28 @@ class ComplimentContainer extends Component {
   render(){
     return (
         <div className="compliment_container">
+          <div className="little_header_container">
+          <img src="../../favicon.ico" alt="Big App Yourself logo"/>
+          <h1>Big App Yourself</h1>
+          </div>
+
         <div className="logout_container">
           <Logout/>
         </div>
-          <div className="greeting">
-            <h1>Hi {this.state.user.firstName}!</h1>
-          </div>
+
           <div className="compliment">
+
+              <h1>Hi {this.state.user.firstName}!</h1>
+
             <h2>{this.state.user.compliment}</h2>
           </div>
           <div className="praise_button">
-            <button className="praise_me" onClick={this.handleBigAppButtonClick}><img src="../../favicon.ico" alt="Big App Yourself"/><p>Click for praise</p></button>
+            <button className="praise_me" onClick={this.handleBigAppButtonClick}><p><strong>Click for praise</strong></p></button>
           </div>
 
           <form className="tweet-mate">
-            <label htmlFor="twitter_handle"><h3 className="tweet-spiel">Enjoy our compliments? Why not Big App a friend and share the love? Just enter your pal's Twitter handle below, and we'll compliment them for you.</h3></label>
-            <input class="twitter_handle" type="text" name="twitter_handle" placeholder="Enter a pal's Twitter handle here" onChange={this.handleFriend} value={this.state.friendsTwitter}/>
+            <label htmlFor="twitter_handle"><h3 className="tweet-spiel">Enjoy our compliments? Why not Big App a friend and share the love? Just enter your pal's Twitter handle below, click that blue button, and we'll compliment them for you.</h3></label>
+            <input className="twitter_handle" type="text" name="twitter_handle" placeholder="Enter a pal's Twitter handle here" onChange={this.handleFriend} value={this.state.friendsTwitter}/>
             <button className="send_tweet" onClick={this.handleTweetYourFriend}><p>Tweet my mate!</p></button>
           </form>
 
